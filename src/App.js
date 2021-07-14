0bd382a7a79b999c49b7e99c1d0eeecd53ca0eb1
Roma Kokontsev <Romeo42@ukr.net>
2021-07-14 23:17:14 +0300

    добавляет кнопки отзывов и статистику

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper/Wrapper";
+import Section from "./components/Section/Section";
+import OptionsFeedBack from "./components/OptionsFeedback/OptionsFeedback";
+import Statistics from "./components/Statistics/Statistics";
+import Notification from "./components/Notification/Notification";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const total = Object.values(this.state).reduce((acc, el) => acc + el, 0);
+    return total;
+  };
+
+  countPositiveFeedback = () => {
+    const total = this.countTotalFeedback();
+    const persantage = Math.round((100 / total) * this.state.good);
+    return persantage;
+  };
+
+  addFeedback = (key) => {
+    this.setState((prevState) => ({
+      [key]: prevState[key] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <Wrapper>
+        <Section title="Please leave feedback">
+          <OptionsFeedBack
+            options={Object.keys(this.state)}
+            clickFeedback={this.addFeedback}
+          ></OptionsFeedBack>
+        </Section>
+
+        <Section title="Statistic">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedback()}
+            ></Statistics>
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
